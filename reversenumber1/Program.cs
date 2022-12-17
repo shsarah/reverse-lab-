9b49935d9455cb13160bc0855c8e3081e15d5782
shsarah <saraash@code.edu.az>
2022-12-17 22:19:07 +0400

    reverse

---
+++ reversenumber1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace reversenumber1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Eded daxil edin:");
+        int n = int.Parse(Console.ReadLine());
+        int s = 0;
+        int reverse = 0;
+        while (n > 0)
+        {
+            int c = n % 10;
+            reverse = reverse * 10 + c;
+            n /= 10;
+        }
+        Console.WriteLine(reverse);
+
+    }
+}
+
